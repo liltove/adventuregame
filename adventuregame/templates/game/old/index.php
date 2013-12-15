8db1cf029fbcd6fda8238b2c844b74c188fe3234
liltove <kringlan@uci.edu>
2013-12-14 21:38:57 -0800

    no more php

---
+++ adventuregame/templates/game/old/index.php
@@ -0,0 +1,29 @@
+{% load staticfiles %}
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Frontier</title>
+<link rel="stylesheet" type="text/css" href="{% static 'game/main.css' %}" />
+</head>
+
+<body>
+
+<div id="welcomeBar">
+	<h1>Welcome to The Frontier,
+	Frontier Fracas,
+	Lock, Stock, and Two Smoking Frontiers (... wtf does that even mean),
+	Guess who’s coming to … the frontier!,
+	Space, The Penultimate Frontier!</h1>
+</div>
+<div id ="main">
+	<div id="nav">
+		{% static 'game/nav.php' %}
+		<?php include("/static/game/nav.php"); ?>
+	</div>
+	<div id="win"
+		<p>More to come soon.</p>
+	</div>
+</div>
+</body>
+</html>
