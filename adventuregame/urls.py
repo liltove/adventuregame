fe680ddc6e1c18eaba9d3375fd9f14b2b02ef5f7
liltove <kringlan@uci.edu>
2013-12-14 18:38:16 -0800

    game view

--- adventuregame/urls.py
+++ adventuregame/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'adventuregame.views.home', name='home'),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', 'game.views.index', name='index'),
 )
--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-from django.conf.urls import patterns, url
-
 from game import views
 
 urlpatterns = patterns('',
