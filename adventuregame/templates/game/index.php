beb1ac363bd0d31f339920deb39834124855903e
liltove <kringlan@uci.edu>
2013-12-14 21:00:54 -0800

    nav menu

--- adventuregame/templates/game/index.php
+++ adventuregame/templates/game/index.php
@@ -19,7 +19,7 @@
 <div id ="main">
 	<div id="nav">
 		<?php include "{% static 'game/nav.php' %}"; ?>
-		{% static %}
+		{% static 'game/nav.php' %}
 	</div>
 	<div id="win"
 		<p>More to come soon.</p>
