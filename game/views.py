991843bc398270de4aeb2c55e619d3ae02dc9c48
liltove <kringlan@uci.edu>
2013-12-14 19:11:08 -0800

    game view

--- game/views.py
+++ game/views.py
@@ -2,11 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-class MainView():
-
-    """
-    A view of the main game page.
-    """
-    # We cannot easily override FormView since this view uses multiple forms
-
-    template_name = "game/index.html"
+def index(request):
+	return render(request, 'game/index.html', context)
