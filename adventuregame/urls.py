a6bd98208224cfdb05abed9f044da1f1cd13d261
liltove <kringlan@uci.edu>
2013-12-14 18:56:43 -0800

    game view

--- adventuregame/urls.py
+++ adventuregame/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'adventuregame.views.home', name='home'),
+    #url(r'^$', 'adventuregame.views.home', name='home'),
     #url(r'^blog/', views.index, name='index'),
-    url(r'^blog/', include('game.urls', namespace="game")),
+    url(r'^game/', include('game.urls', namespace="game")),
 )
