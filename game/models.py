ba5f68505b8a08c4cfb89a709c36ec04420238fc
liltove <kringlan@uci.edu>
2013-12-23 22:37:40 -0800

    created base PC characteristics

--- game/models.py
+++ game/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 class PlayerChar(models.Model):
-	name = models.CharField(max_length=128, unique=True)
+	NAME = models.CharField(max_length=128, unique=True)
+	
+	FEMALE = 'Female'
+	MALE = 'Male'
+	GENDER_CHOICES = (
+		(FEMALE, 'Female'),
+		(MALE, 'Male'),
+	)
+	GENDER = models.CharField(max_length=2,
+								choice=GENDER_CHOICES,
+								default=FEMALE)
+
+	AGE = models.CharInteger(max_length=3,
+								default=18)
 
 	def ___unicode__(self):
 		return self.name
