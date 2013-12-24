025491f3fcbe103509c132895a2f5a22a6480588
liltove <kringlan@uci.edu>
2013-12-23 22:48:21 -0800

    trying to find name error

--- game/models.py
+++ game/models.py
@@ -4,18 +4,17 @@ from django.db import models
 class PlayerChar(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	
-	FEMALE = 'Female'
-	MALE = 'Male'
-	GENDER_CHOICES = (
-		(FEMALE, 'Female'),
-		(MALE, 'Male'),
-	)
-	gender = models.CharField(max_length=2,
-								choices=GENDER_CHOICES,
-								default=FEMALE)
+	#FEMALE = 'Female'
+	#MALE = 'Male'
+	#GENDER_CHOICES = (
+	#	(FEMALE, 'Female'),
+	#	(MALE, 'Male'),
+	#)
+	#gender = models.CharField(max_length=2,
+	#							choices=GENDER_CHOICES,
+	#							default=FEMALE)
 
-	age = models.IntegerField(max_length=3,
-								default=18)
+	#age = models.IntegerField(default=18)
 
 	def ___unicode__(self):
 		return self.name
