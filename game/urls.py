2d233c18e32998f35117a30542a4d93b5fb0f500
liltove <kringlan@uci.edu>
2013-12-23 22:19:45 -0800

    more admin

--- game/urls.py
+++ game/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import patterns, include, url
 
 from game import views
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
 )
