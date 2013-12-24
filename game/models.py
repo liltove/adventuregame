362fce34baf2e6262af62355f4a90b0deb7ade74
liltove <kringlan@uci.edu>
2013-12-23 22:53:10 -0800

    still trying to fix

--- game/models.py
+++ game/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class PlayerChar(models.Model):
-	name = models.CharField(max_length=128, unique=True)
+	pcname = models.CharField(max_length=128, unique=True)
 	
 	#FEMALE = 'Female'
 	#MALE = 'Male'
@@ -17,4 +17,10 @@ class PlayerChar(models.Model):
 	#age = models.IntegerField(default=18)
 
 	def ___unicode__(self):
-		return self.name
+		return self.pcname
+
+class ItemsOfWorld(models.Model):
+	pcname = models.CharField(max_length=128, unique=True)
+
+	def __unicode__(self):
+		return self.pcname
