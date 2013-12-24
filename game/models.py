441bf3e46dd2df773b7e8d6575fd2b39dc1895fd
liltove <kringlan@uci.edu>
2013-12-23 23:01:11 -0800

    trying to get name error fixed

--- game/models.py
+++ game/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class PlayerChar(models.Model):
-	pcname = models.CharField(max_length=128, unique=True)
+	name = models.CharField(max_length=128, unique=True)
 	
 	#FEMALE = 'Female'
 	#MALE = 'Male'
@@ -17,10 +17,10 @@ class PlayerChar(models.Model):
 	#age = models.IntegerField(default=18)
 
 	def ___unicode__(self):
-		return self.pcname
+		return self.name
 
 class ItemsOfWorld(models.Model):
-	pcname = models.CharField(max_length=128, unique=True)
+	name = models.CharField(max_length=128, unique=True)
 
 	def __unicode__(self):
-		return self.pcname
+		return self.name
